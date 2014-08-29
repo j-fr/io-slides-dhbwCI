e848f7e2f3f127844b47c7da64701284b0448532
Jonas Fritsche <fritsche@freinet.de>
2014-08-29 11:13:25 +0200

    Initial changes, variables, DHBW CI

--- slide_config.js
+++ slide_config.js
@@ -1,12 +1,12 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Lean Software Engineering',
+    subtitle: 'Präsentation im Rahmen der 2. PA im Studiengang WWI',
+    eventInfo: {
+      title: 'Präsentation PAII',
+      date: '29.08.2014'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
@@ -14,7 +14,7 @@ var SLIDE_CONFIG = {
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
     favIcon: 'images/google_developers_logo_tiny.png',
     fonts: [
-      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Droid Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
@@ -22,11 +22,11 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
+    name: 'Jonas Fritsche',
+    company: 'Softwareentwickler, FreiNet GmbH',
     gplus: 'http://plus.google.com/1234567890',
     twitter: '@yourhandle',
-    www: 'http://www.you.com',
+    www: 'http://www.freinet.de',
     github: 'http://github.com/you'
   }/*, {
     name: 'Second Name',
